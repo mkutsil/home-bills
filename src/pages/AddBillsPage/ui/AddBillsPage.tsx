143395dd031b470df9a50354f132dd6924cf2f69
Mykola Kutsil <mkutsil.dev@gmail.com>
2026-01-29 22:57:04 +0200

    feature: added func

--- src/pages/AddBillsPage/ui/AddBillsPage.tsx
+++ src/pages/AddBillsPage/ui/AddBillsPage.tsx
@@ -1,3 +1,24 @@
+import { useState, ChangeEvent } from 'react';
+
 export const AddBillsPage = () => {
-    return <h1>AddBillsPage</h1>;
+    const [inputValue, setInputValue] = useState('');
+    const [billsValue, setBillsValue] = useState('');
+
+    const handleInputChange = (e: ChangeEvent<HTMLInputElement>) => {
+        setInputValue(e.target.value);
+    };
+
+    const handleSaveClick = () => {
+        setBillsValue(inputValue);
+        setInputValue('');
+        localStorage.setItem('billsValue', inputValue);
+    };
+    return (
+        <>
+            <h1>Введіть показники лічильника</h1>
+            <input type="number" value={inputValue} onChange={handleInputChange} />
+            <button onClick={handleSaveClick}>Зберегти</button>
+            {billsValue && <p>Збережені показники: {billsValue}</p>}
+        </>
+    );
 };
--- src/pages/HomePage/ui/HomePage.tsx
+++ src/pages/HomePage/ui/HomePage.tsx
@@ -1,1 +1,9 @@
-export const HomePage = () => <h1>Home page</h1>;
+export const HomePage = () => {
+    return (
+        <>
+            <h1>Home page</h1>
+
+            <p>показникик {localStorage.getItem('billsValue')}</p>
+        </>
+    );
+};
