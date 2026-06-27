2d35c8b56d79b05faeead3d622daec35e752bd43
Mykola Kutsil <mkutsil.dev@gmail.com>
2026-06-27 09:32:52 +0300

    feature: added Tailwind CSS and shadcn ui

--- src/main.tsx
+++ src/main.tsx
@@ -4,6 +4,7 @@ import App from './app/App';
 import { BrowserRouter } from 'react-router-dom';
 import { store } from './app/store/store';
 import { Provider } from 'react-redux';
+import './main.css';
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
